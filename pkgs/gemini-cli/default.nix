701a4440964af525187ff753f1eca04e6c973977
Samir Ettali <s.ettali@young.business>
2025-07-04 22:25:57 +0200

    Revert "gemini-cli: 0.1.7 -> 0.1.10"
    This reverts commit 45d1e09ab1c239581152a72ce7163672616a34cf.

--- pkgs/gemini-cli/default.nix
+++ pkgs/gemini-cli/default.nix
@@ -13,18 +13,18 @@
 }:
 buildNpmPackage (finalAttrs: {
   pname = "gemini-cli";
-  version = "0.1.10";
+  version = "0.1.7";
 
   src = fetchFromGitHub {
     owner = "google-gemini";
     repo = "gemini-cli";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-kagRpR6iYM1jGwmNl92IYUkHHiske+v0uQETLuOrRos=";
+    hash = "sha256-DAenod/w9BydYdYsOnuLj7kCQRcTnZ81tf4MhLUug6c=";
   };
 
   npmDeps = fetchNpmDeps {
     inherit (finalAttrs) src;
-    hash = "sha256-7jqNdTwXZ9/sIeFSxaoTo3ak5TgcIp+NJsql26ziyAo=";
+    hash = "sha256-otogkSsKJ5j1BY00y4SRhL9pm7CK9nmzVisvGCDIMlU=";
   };
 
   preConfigure = ''
